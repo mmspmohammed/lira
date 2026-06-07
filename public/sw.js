7d8f3465760608cfe1481e86ca4aab63b69089c2
mmspmohammed <mohyoss6655@gmail.com>
2026-06-07 23:52:20 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "lirasyp-v1";
+const STATIC_ASSETS = ["/", "/prices/currency", "/prices/gold", "/news"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      return cached || fetch(e.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("push", (e) => {
+  const data = e.data.json();
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icon-192x192.png",
+      data: { url: data.url },
+    })
+  );
+});
